791f485df213e6b867e25aeb12b7c992f9b91993
deddyprianto <haicoders12@gmail.com>
2024-09-27 15:46:59 +0700

    lol

--- components/Outlet/index.tsx
+++ components/Outlet/index.tsx
@@ -14,6 +14,7 @@ interface OutletParams {
 
 const OutletCP: React.FC<OutletParams> = ({ data }) => {
   const router = useRouter();
+  console.log("kimak");
   return (
     <div>
       {data.map((item) => {
